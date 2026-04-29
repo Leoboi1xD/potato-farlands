24cb59531873f23b5f4551804da0d4be86eb29f1
Leoboi1xD <leonidas.anwar@gmail.com>
2026-04-29 20:14:58 +0200

    Initial commit

---
+++ src/main/java/net/farlands/potatofarlands/mixin/ExampleMixin.java
@@ -0,0 +1,11 @@
+package net.farlands.potatofarlands.mixin;
+
+import net.minecraft.util.math.noise.OctavePerlinNoiseSampler;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
+
+@Mixin(OctavePerlinNoiseSampler.class)
+public class ExampleMixin {
+	@Overwrite
+    public static double maintainPrecision (double d) {return d;}
+}
